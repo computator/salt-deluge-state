61b244d5d1e55c9cbbbed065b194bf25cdab5ed4
Reuben Lifshay <rlifshay@gmail.com>
2016-11-06 01:54:39 -0700

    Check whether yarss is available before calling it and add a method to check if it's available.

--- _modules/deluge_yarss.py
+++ _modules/deluge_yarss.py
@@ -54,10 +54,30 @@ if(DELUGE_EXISTS):
 	reactor_thread.daemon = True
 	reactor_thread.start()
 
-# end of helpers
+# end of deluge related helpers
 
 def __virtual__():
 	return True if DELUGE_EXISTS else (False, "The deluge_yarss module cannot be loaded: deluge is not installed.")
+
+def _check_yarss(ret=False):
+	plugins = _block_on(_reactor_call(client.core.get_enabled_plugins), 120)
+	log.debug("Enabled deluge plugins: %s", plugins)
+	if 'YaRSS2' in plugins:
+		return True
+	log.warn("The deluge YaRSS2 plugin was not found")
+	if not ret:
+		raise CommandExecutionError("The deluge YaRSS2 plugin is not available")
+	return False
+
+def is_available(**connection_args):
+	'''
+	Checks if the deluge YaRSS2 plugin is is_available
+	'''
+	try:
+		with _Connection(**{k[11:]: v for k, v in connection_args.iteritems() if k.startswith('connection_')}):
+			return _check_yarss(True)
+	except Exception as e:
+		raise CommandExecutionError('Error calling deluge: {0}'.format(get_error_message(e)))
 
 def get_subscriptions(**connection_args):
 	'''
@@ -65,6 +85,7 @@ def get_subscriptions(**connection_args):
 	'''
 	try:
 		with _Connection(**{k[11:]: v for k, v in connection_args.iteritems() if k.startswith('connection_')}):
+			_check_yarss()
 			config = _block_on(_reactor_call(client.yarss2.get_config), 120)
 		return config['subscriptions']
 	except Exception as e:
