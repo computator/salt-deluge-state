2f6a279e3d142c6c6a5a7d40102affe3a8d8e8ae
Reuben Lifshay <rlifshay@gmail.com>
2016-11-21 00:00:06 -0800

    Add feed method to deluge_yarss state module.

--- _states/deluge_yarss.py
+++ _states/deluge_yarss.py
@@ -1,6 +1,66 @@
 import logging
 log = logging.getLogger(__name__)
 
+def feed(name, url, **kwargs):
+	'''
+	Make sure a RSS feed exists with the specified values
+
+	name
+		The RSS feed title.
+	url
+		The RSS feed URL.
+
+	Options that start with 'connection_' are used to connect to the deluge daemon. All other options override the corresponding subscription value.
+	'''
+
+	ret = {
+			'name': name,
+			'result': False,
+			'changes': {},
+			'comment': '',
+		}
+
+	conn_args = {}
+	for k in kwargs:
+		if k.startswith('connection_'):
+			conn_args[k] = kwargs.pop(k)
+
+	feed_state = kwargs
+	if 'key' in feed_state:
+		del feed_state['key']
+	feed_state.update({
+			'name': name,
+			'url': url,
+		})
+
+	curr_feed = __salt__['deluge_yarss.get_feed'](name, **conn_args)
+	if curr_feed:
+		log.debug("Found matching RSS feed: %s", curr_feed)
+	else:
+		log.debug("No existing RSS feed found matching '%s'", name)
+
+	if curr_feed and len(feed_state) == len(feed_state.viewitems() & {k: curr_feed[k] for k in curr_feed if k != 'key'}.viewitems()):
+		ret.update(comment="RSS feed already up to date", result=True)
+		return ret
+
+	if curr_feed:
+		new_state = curr_feed.copy()
+		new_state.update(feed_state)
+		changes = {k: {'old': curr_feed[k], 'new': v} for k, v in new_state.viewitems() - curr_feed.viewitems()}
+	else:
+		new_state = feed_state
+		changes = {k: {'old': None, 'new': v} for k, v in new_state.iteritems()}
+
+	if __opts__['test']:
+		ret.update(comment="RSS feed will be " + ("updated" if curr_feed else "added"), pchanges=changes, result=None)
+		return ret
+
+	log.debug("Calling set_feed with values: %s", new_state)
+	__salt__['deluge_yarss.set_feed'](new_state, **conn_args)
+
+	ret.update(comment="RSS feed was " + ("updated" if curr_feed else "added"), changes=changes, result=True)
+	return ret
+
 def subscription(name, regex, feed_id, **kwargs):
 	'''
 	Make sure a subscription exists with the specified values
