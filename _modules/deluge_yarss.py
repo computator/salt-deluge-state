3be74980692120bd3ffa196a21e677e1ea4166bd
Reuben Lifshay <rlifshay@gmail.com>
2016-11-21 02:20:10 -0800

    Attempt to verify subscription and feed data before saving.

--- _modules/deluge_yarss.py
+++ _modules/deluge_yarss.py
@@ -11,6 +11,84 @@ try:
 except ImportError:
 	DELUGE_EXISTS = False
 
+try:
+	from yarss2.yarss_config import get_fresh_rssfeed_config, get_fresh_subscription_config, _verify_types
+	log.info("Successfully imported YaRSS2 default config methods")
+
+	def _verify_feed(feed):
+		has_key = 'key' in feed
+		_verify_types(feed['key'] if has_key else "", feed, get_fresh_rssfeed_config())
+		if not has_key and 'key' in feed:
+			del feed['key']
+		return feed
+
+	def _verify_subscr(subscr):
+		has_key = 'key' in subscr
+		_verify_types(subscr['key'] if has_key else "", subscr, get_fresh_subscription_config())
+		if not has_key and 'key' in subscr:
+			del subscr['key']
+		return subscr
+
+except ImportError:
+	log.warn("Failed to import YaRSS2 default config methods. Falling back to internal defaults with limited error checking.")
+
+	def get_fresh_rssfeed_config(name=u"", url=u"", site=u"", active=True, last_update=u"",
+								 update_interval=120, update_on_startup=False,
+								 obey_ttl=False, user_agent=u"", key=None):
+		"""Create a new config (dictionary) for a feed"""
+		config_dict = {}
+		config_dict["name"] = name
+		config_dict["url"] = url
+		config_dict["site"] = site
+		config_dict["active"] = active
+		config_dict["last_update"] = last_update
+		config_dict["update_interval"] = update_interval
+		config_dict["update_on_startup"] = update_on_startup
+		config_dict["obey_ttl"] = obey_ttl
+		config_dict["user_agent"] = user_agent
+		config_dict["prefer_magnet"] = False
+		if key:
+			config_dict["key"] = key
+		return config_dict
+
+	def get_fresh_subscription_config(name=u"", rssfeed_key="", regex_include=u"", regex_exclude=u"",
+									  active=True, move_completed=u"", download_location=u"", last_match=u"",
+									  label=u"", ignore_timestamp=False, key=None):
+		"""Create a new config """
+		config_dict = {}
+		config_dict["rssfeed_key"] = rssfeed_key
+		config_dict["regex_include"] = regex_include
+		config_dict["regex_include_ignorecase"] = True
+		config_dict["regex_exclude"] = regex_exclude
+		config_dict["regex_exclude_ignorecase"] = True
+		config_dict["name"] = name
+		config_dict["active"] = active
+		config_dict["last_match"] = last_match
+		config_dict["ignore_timestamp"] = False
+		config_dict["move_completed"] = move_completed
+		config_dict["download_location"] = download_location
+		config_dict["custom_text_lines"] = u""
+		config_dict["email_notifications"] = {}  # Dictionary where keys are the keys of email_messages dictionary
+		config_dict["max_download_speed"] = -2
+		config_dict["max_upload_speed"] = -2
+		config_dict["max_connections"] = -2
+		config_dict["max_upload_slots"] = -2
+		config_dict["add_torrents_in_paused_state"] = u"Default"
+		config_dict["auto_managed"] = u"Default"
+		config_dict["sequential_download"] = u"Default"
+		config_dict["prioritize_first_last_pieces"] = u"Default"
+		config_dict["label"] = label
+
+		if key is not None:
+			config_dict["key"] = key
+		return config_dict
+
+	def _verify_feed(feed):
+		return get_fresh_rssfeed_config(**feed)
+
+	def _verify_subscr(subscr):
+		return get_fresh_subscription_config(**subscr)
+
 class BlockingTimeout(RuntimeError): pass
 
 def _block_on(d, timeout=None):
@@ -150,6 +228,7 @@ def set_subscription(data, **connection_args):
 	Updates an existing entry if data['key'] is set
 	'''
 	try:
+		data = _verify_subscr(data)
 		with _Connection(**{k[11:]: v for k, v in connection_args.iteritems() if k.startswith('connection_')}):
 			_check_yarss()
 			config = _block_on(_reactor_call(client.yarss2.save_subscription, subscription_data=data), 120)
@@ -163,6 +242,7 @@ def set_feed(data, **connection_args):
 	Updates an existing entry if data['key'] is set
 	'''
 	try:
+		data = _verify_feed(data)
 		with _Connection(**{k[11:]: v for k, v in connection_args.iteritems() if k.startswith('connection_')}):
 			_check_yarss()
 			config = _block_on(_reactor_call(client.yarss2.save_rssfeed, rssfeed_data=data), 120)
